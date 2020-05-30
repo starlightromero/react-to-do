538911239c52f095ef77e562f5e8d5d4a20e2517
Starlight Romero <starlightromero@gmail.com>
2020-05-30 06:24:14 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      todos: [],
+      userInput: ''
+    }
+  }
+
+  userInputChanged = event => {
+    this.setState({
+      userInput: event.target.value
+    })
+  }
+
+  addTodo = () => {
+    const newTodo = {
+      id: Math.random(),
+      name: this.state.userInput,
+      completed: false
+    }
+    this.setState({ todos: [newTodo, ...this.state.todos] })
+  }
+
+  changeState = (id, completed) => {
+    if (completed) {
+      this.setState({
+        todos: [...this.state.todos.filter(todo => todo.id !== id)]
+      })
+    } else {
+      this.setState({
+        todos: this.state.todos.map((todo, index) => {
+          if (todo.id === id) {
+            todo.completed = !todo.completed
+          }
+          return todo
+        }).sort((a, b) => a.completed - b.completed)
+      })
+    }
+  }
+
+  render () {
+    let todos = this.state.todos.map((todo, index) => {
+      let completed = () => todo.completed ? 'completed' : ''
+      return (
+        <li
+          key={index}
+          class={completed()}
+          onClick={this.changeState.bind(this, todo.id, todo.completed)}>
+          {todo.name}
+        </li>
+      )
+    })
+    return (
+      <div className='App'>
+        <h1>React To-Do</h1>
+        <input
+          type='text'
+          placeholder='Add To Do...'
+          value={this.state.userInput}
+          onChange={this.userInputChanged.bind(this)}
+        />
+        <button onClick={this.addTodo.bind(this)}>Add {this.state.userInput}</button>
+        <hr />
+        <ul>
+          {todos}
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
